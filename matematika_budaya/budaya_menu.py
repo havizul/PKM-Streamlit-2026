f11d29969b315548dab6b1c7fc5fdf48e7252444
Havizul <havizul@gmail.com>
2026-09-22 09:13:31 +0700

    Update budaya_menu.py

--- matematika_budaya/budaya_menu.py
+++ matematika_budaya/budaya_menu.py
@@ -437,7 +437,7 @@ def bubu():
 
     for i, item in enumerate(soal):
 
-        st.markdown(f"### {item['soal']}")
+        st.markdown(f"{item['soal']}")
 
         jawaban = st.radio(
             "Pilih jawaban:",
